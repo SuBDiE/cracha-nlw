d279c989477c8fee6034bfc46bdc844d411c5c4c
Victor Matthaus <65428226+SuBDiE@users.noreply.github.com>
2021-10-21 21:11:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'SuBDiE',
+  youtube: 'Curso em video',
+  facebook: 'Victormpraia',
+  instagram: 'siegermatth',
+  twitter: 'VictorMpraia'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserAvatar.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
